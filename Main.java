a0fd367814ae14d21556d72623603adf47a15da5
John Rocha <johnigorj@gmail.com>
2026-09-01 20:50:29 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        double altura;
+        double peso;
+        double IMC;
+        System.out.println("Digite seu peso em Kg: ");
+        peso =  input.nextDouble();
+        System.out.println("Digite sua altura em metros:" );
+        altura = input.nextDouble();
+
+        IMC = peso/altura * altura;
+        System.out.println("O IMC calculado foi:"+ " "+ IMC);
+    }
+}
