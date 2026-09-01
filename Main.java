ddf51599edbf70095e971be151acdfc69227a084
John Rocha <johnigorj@gmail.com>
2026-09-01 20:24:58 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        double PriN;
+        double dobro;
+
+             System.out.println("Digite um número:");
+                PriN = input.nextDouble();
+                dobro = PriN*2;
+            System.out.println("O dobro do número digitado é:"+ ""+ dobro);
+
+        }
+}
+
