91294b280e40f9f50e2d08d34d91cebb3134c222
John Rocha <johnigorj@gmail.com>
2026-09-21 15:55:28 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int quantidade;
+        System.out.println("Olá. Digite o número de vezes que a mensagem será exibida:");
+        quantidade = input.nextInt();
+        for ( int i =1; i <= quantidade; i++){
+
+            System.out.println("Praticando lógica de programação");
+
+
+    }
+    }
+}
